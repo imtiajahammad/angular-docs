3e7bd43486c784a0ca1b6bf95413d337bcb7ca59
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-01-25 17:32:25 +0600

    new sampleProject added

---
+++ sampleProject/src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+
+import { products } from '../products';
+
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+}
