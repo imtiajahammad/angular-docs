378ed5770d5f11505ed6844fe59fc4083062b202
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-02-06 01:33:07 +0600

    Managing Data

--- sampleProject/src/app/app.module.ts
+++ sampleProject/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +19,19 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
-  imports: [
+  imports: [ 
     BrowserModule
+    ,HttpClientModule
     ,ReactiveFormsModule
     , RouterModule.forRoot([
       { path: '', component:ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping' , component: ShippingComponent },
     ])
   ],
   providers: [],
