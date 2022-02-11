af88ab4561c8a40d23ddf590d5683085b090ff59
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-02-11 17:24:04 +0600

    No encapsulation

--- sampleProject/src/app/app.module.ts
+++ sampleProject/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ShippingComponent } from './shipping/shipping.component';
+import { NoEncapsulationComponent } from './no-encapsulation/no-encapsulation.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
-    ShippingComponent
+    ShippingComponent,
+    NoEncapsulationComponent
   ],
   imports: [ 
     BrowserModule
