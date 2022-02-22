6fd60e72c1478ffa3309840c3ed0f0063127a222
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-02-22 11:46:28 +0600

    Intercept input property changes with ngOnChanges()

---
+++ sampleProject/src/app/version-child/version-child.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-version-child',
+  templateUrl: './version-child.component.html',
+  styleUrls: ['./version-child.component.css']
+})
+export class VersionChildComponent implements OnInit {
+
+  @Input() major = 0;
+  @Input() minor = 0;
+  changeLog: string[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    const log: string[] = [];
+    for (const propName in changes) {
+      const changedProp = changes[propName];
+      const to = JSON.stringify(changedProp.currentValue);
+      if (changedProp.isFirstChange()) {
+        log.push(`Initial value of ${propName} set to ${to}`);
+      } else {
+        const from = JSON.stringify(changedProp.previousValue);
+        log.push(`${propName} changed from ${from} to ${to}`);
+      }
+    }
+    this.changeLog.push(log.join(', '));
+  }
+
+}
