c2f913a00fadd3b7fd1b26e80bc53e790589062b
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2022-02-23 11:00:01 +0600

    Parent calls an @ViewChild()

--- sampleProject/src/app/countdown-parent/countdown-parent.component.ts
+++ sampleProject/src/app/countdown-parent/countdown-parent.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CountdownTimerComponent } from '../countdown-timer/countdown-timer.component';
+import { AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-countdown-parent',
@@ -7,6 +8,21 @@ import { CountdownTimerComponent } from '../countdown-timer/countdown-timer.comp
   styleUrls: ['./countdown-parent.component.css']
 })
 export class CountdownParentComponent implements OnInit {
+
+  @ViewChild(CountdownTimerComponent)
+  private timerComponent!: CountdownTimerComponent;
+
+  seconds() { return 0; }
+
+  ngAfterViewInit() {
+    // Redefine `seconds()` to get from the `CountdownTimerComponent.seconds` ...
+    // but wait a tick first to avoid one-time devMode
+    // unidirectional-data-flow-violation error
+    setTimeout(() => this.seconds = () => this.timerComponent.seconds, 0);
+  }
+
+  start() { this.timerComponent.start(); }
+  stop() { this.timerComponent.stop(); }
 
   constructor() { }
 
